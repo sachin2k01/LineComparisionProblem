af4eb456feae8fc6bad0a828e44eed0416f787d7
sachin2k01 <thenamesachin@gmail.com>
2023-11-30 23:07:41 +0530

    initial commit

---
+++ src/com/bridgelabz/linecompare/LineCompare.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecompare;
+
+public class LineCompare
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Problem");
+    }
+}
